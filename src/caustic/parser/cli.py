b772a4ba8d2141909e999eca435dbfbefe05ab16
Shae <shae.m.vc@proton.me>
2024-05-02 10:25:47 -0700

    Implemented `imports` subcommand

--- src/caustic/parser/cli.py
+++ src/caustic/parser/cli.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 #> Imports
+import json
 import click
 import parglare
 from enum import IntEnum
@@ -35,12 +36,31 @@ class SharedState:
         else: gp = self.grammar_path
         return parglare.Grammar.from_file(gp)
 
-@click.group()
+@click.group
 @click.option('-g', '--grammar', type=click.Path(exists=True, dir_okay=False, path_type=Path), default=None,
               help='The grammar file to load (defaults to the built-in canonical grammar, if present)')
 @click.pass_context
 def cap(ctx: click.Context, *, grammar: Path | None) -> None:
     ctx.obj = SharedState(grammar_path=grammar)
+
+@cap.command
+@click.pass_obj
+@click.option('-j', '--json', 'as_json', is_flag=True, default=False,
+              help='Output as JSON')
+def imports(state: SharedState, *, as_json: bool) -> None:
+    gram = state.load_grammar()
+    if as_json:
+        click.echo(json.dumps(
+            {f: {n: i.file_path for n,i in sg.imports.items()}
+             for f,sg in gram.imported_files.items()}, indent=4))
+        return
+    for f,sg in gram.imported_files.items():
+        click.echo(f'{f}:', nl=False)
+        if sg.imports:
+            click.echo()
+            for n,i in sg.imports.items():
+                click.echo(f'- {n} @ {i.file_path}')
+        else: click.echo(' <no imports>')
 
 
 if __name__ == '__main__': cap()
