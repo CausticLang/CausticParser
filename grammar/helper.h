f48c3e521eaeaa74a6460477796f47083f4d5c99
Shae <shae.m.vc@proton.me>
2024-05-14 23:23:38 -0700

    Moved helper macros from `parser.peg` to `grammar/helper.h`

---
+++ grammar/helper.h
@@ -0,0 +1,17 @@
+#define _cap_PCC0 _0
+#define _cap_PCC0s _0s
+#define _cap_PCC0e _0e
+
+#define _cap_COPYSTR(frm) \
+    strcpy(malloc(((strlen(frm) + 1) * sizeof(char))), frm)
+#define _cap_ERROR(rname, msg) \
+    cap_error(rname, msg, _cap_PCC0, _cap_PCC0s, _cap_PCC0e)
+
+#define _cap_MKNODE(type) \
+    (struct cst_n##type*)malloc(sizeof(struct cst_n##type))
+#define _cap_MKINITNODE(type, ...) \
+    cst_ninit_##type(_cap_MKNODE(type), _cap_PCC0s, _cap_PCC0e, __VA_ARGS__)
+
+#define _cap_ADDNODE(node) \
+    cst_node_add(auxil, cst_NODEDOWNCAST(node))
+#define _cap_LASTNODE auxil->node_count-1
