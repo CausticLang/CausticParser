a55d49ed12c39e3751ad880df9e03bac78f4b812
Shae <shae.m.vc@proton.me>
2024-05-15 13:32:37 -0700

    Implemented experimental better error messages

--- src/error.c
+++ src/error.c
@@ -1,15 +1,20 @@
 #include "error.h"
 
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 
 #include "parser_state.h"
 
 void cap_error(struct cap_ParserConfig* conf, const char* rule, const char* msg, const char* input, unsigned int start_pos, unsigned int end_pos) {
-    if (!conf->suppress_error_msgs)
-        fprintf(cap_ERROR_STREAM, cap_ERROR_FMT, rule, msg, input, start_pos, end_pos);
-    if (!conf->error_recover)
-        exit(1);
+    if (!conf->suppress_error_msgs) {
+        fprintf(cap_ERROR_STREAM, "Syntax error (%s): %s\n%s\n%*c", rule, msg, input, start_pos, '^');
+        if (start_pos == end_pos) fputc('\n', cap_ERROR_STREAM);
+        else fprintf(cap_ERROR_STREAM, "%*c\n", end_pos-start_pos, '^');
+    }
+    if (!conf->error_recover) exit(1);
 }
 
-void cap_debug(struct cap_ParserState* state, const char* event, const char* rule, const int event, const char* buffer, const int length)
+void cap_debug(struct cap_ParserState* state, const int event, const char* rule, const int level, const int pos, const char* buffer, const int length) {
+    assert(false); // TBA
+}
