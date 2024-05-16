fa6c6b2931e4e437c8ffebc4489e0c85e6a38020
Shae <shae.m.vc@proton.me>
2024-05-16 12:41:41 -0700

    Indev attempt to print context with errors

--- src/error.c
+++ src/error.c
@@ -6,11 +6,45 @@
 
 #include "parser_state.h"
 
-void cap_error(struct cap_ParserState* state, const char* rule, const char* msg, const char* input, unsigned int start_pos, unsigned int end_pos) {
+/*unsigned int _cap_print_until(FILE* stream, char* start, char stop) {
+    fprintf(stream, start);
+    char c = fgetc(stream);
+    unsigned int len = 0;
+    for (; (c != stop) && (c != '\0'); len++) {
+        fputc(c, stream);
+        c = fgetc(stream);
+        assert(len < 20);
+    }
+    return len;
+}*/
+
+void cap_error(struct cap_ParserState* state, cap_context_t* ctx, const char* rule, const char* msg, unsigned int start_pos, unsigned int end_pos) {
     if (!state->config.suppress_error_msgs) {
-        fprintf(cap_ERROR_STREAM,
-                "Syntax error (%3$s) at l%4$u:c%1$u: %5$s\n----------------------------------------\n%6$s\n%2$*c\n",
-                cap_cno(state, start_pos), '^', rule, cap_lno(state), msg, input);
+        unsigned int cno = cap_cno(state, start_pos);
+        fprintf(cap_ERROR_STREAM, "Syntax error (%s) at l%u:c%u: %s\n", //----------------------------------------\n",
+                rule, cap_lno(state), cno, msg);
+        // Print source
+        /// Print line before
+        /*bool pre = false;
+        unsigned int sol = start_pos;
+        for (; sol > 0; sol--)
+            if (ctx->buffer.buf[sol] == '\n') {
+                pre = true;
+                break;
+            }
+        if (pre) {
+            unsigned int solp = sol;
+            for (; solp > 0; solp--)
+                if (ctx->buffer.buf[solp] == '\n') break;
+            _cap_print_until(cap_ERROR_STREAM, ctx->buffer.buf+solp+1, '\n');
+            fputc('\n', cap_ERROR_STREAM);
+        }
+        /// Print current line
+        unsigned int eol = _cap_print_until(cap_ERROR_STREAM, ctx->buffer.buf+sol+1, '\n');
+        //// Print context
+        fprintf(cap_ERROR_STREAM, "\n$*c\n", cno, '^');
+        /// Print line after
+        _cap_print_until(cap_ERROR_STREAM, ctx->buffer.buf+eol+1, '\n');*/
     }
     if (!state->config.error_recover) exit(1);
 }
--- src/error.h
+++ src/error.h
@@ -1,7 +1,9 @@
 #include <assert.h>
-#include "parser_state.h"
 
-void cap_error(struct cap_ParserState* state, const char* rule, const char* msg, const char* input, unsigned int start_pos, unsigned int end_pos);
+#include "parser_state.h"
+#include "../parser.h"
+
+void cap_error(struct cap_ParserState* state, cap_context_t* ctx, const char* rule, const char* msg, unsigned int start_pos, unsigned int end_pos);
 void cap_debug(struct cap_ParserState* state, const int event, const char* rule, const int level, const int pos, const char* buffer, const int length);
 
 #ifndef cap_ERROR_STREAM
