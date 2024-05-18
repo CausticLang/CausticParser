2baafe028882aa7925ce7c0b3ace9c8095c65c8e
Shae <shae.m.vc@proton.me>
2024-05-17 17:55:06 -0700

    Moved `./src/ast/` to `/CausticAST/`

--- src/ast/cst.c
+++
@@ -1,9 +0,0 @@
-#ifndef cst_CST_S_GUARD
-#define cst_CST_S_GUARD 1
-
-#include "cst.h"
-
-#include "node_free.c"
-#include "node_printer.c"
-
-#endif
--- src/ast/cst.h
+++
@@ -1,9 +0,0 @@
-#ifndef cst_CST_H_GUARD
-#define cst_CST_H_GUARD 1
-
-#include "nodes.h"
-
-void cst_free_node(struct cst_NodeBase* node);
-void cst_print_node(struct cst_NodeBase* node, char* prefix, char* suffix);
-
-#endif
