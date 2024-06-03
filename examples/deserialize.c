c4fa9be8ce34c3a9a7f12460512f495347d3ae17
Shae <shae.m.vc@proton.me>
2024-06-03 13:13:57 -0700

    Brought `examples/deserialize.c` up-to-date with latest implementation

--- examples/deserialize.c
+++ examples/deserialize.c
@@ -6,23 +6,21 @@
 #include <malloc.h>
 
 #include "../parser.c"
-#include "../src/ast/serialize.c"
+#include "../src/ast/utils/serialize.c"
 
 int main(void) {
-    struct cst_Root root;
-    root.node_count = 0;
-    root.nodes = NULL;
-    cst_deserialize_to(&root, stdin);
+    cst_Root* root = cst_deserialize_from(stdin);
     // Print nodes
-    for (int i = 0; i < root.node_count; i++) {
+    for (size_t i = 0; i < root->n_nodes; i++) {
         printf("Node@%d:\n", i);
-        cst_print_node(root.nodes[i], " ", "\n");
+        cst_print_node(stdout, root->nodes[i]);
     }
     // Cleanup
-    for (int i = 0; i < root.node_count; i++) {
-        cst_free_node(root.nodes[i]);
-        free(root.nodes[i]);
+    for (int i = 0; i < root->n_nodes; i++) {
+        cst_free_node(root->nodes[i]);
+        free(root->nodes[i]);
     }
-    free(root.nodes);
+    free(root->nodes);
+    free(root);
     return 0;
 }
